f7ec8166aa1856a5cf9020affa223c008473b25b
25LP4018 25LP4018 <25LP4018@G301-CC0190.local>
2025-09-29 09:28:33 +0800

    added:

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -4,10 +4,23 @@ import {
   DetailsDynamicPageComp,
   DetailsDynamicPageSkeleton,
 } from "@/components/details";
+import { getMovieDetails } from "@/utils/get-data";
+import { MovieDetailsType } from "@/types";
 
 type DetailsPageProps = {
   params: Promise<{ id: string }>;
 };
+
+export const generateMetadata = async ({ params }: DetailsPageProps) => {
+  const dynamicParams = await params;
+  const id = dynamicParams.id;
+  const movieDetails: MovieDetailsType = await getMovieDetails(id);
+
+  return {
+    title: `MovieZ | ${movieDetails.title}`,
+  };
+};
+
 const DetailsPage = ({ params }: DetailsPageProps) => {
   return (
     <Suspense fallback={<DetailsDynamicPageSkeleton />}>
--- src/app/genre/page.tsx
+++ src/app/genre/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Suspense } from "react";
 import { GenrePageComp, GenrePageSkeleton } from "@/components/genre";
+import { getGenresList } from "@/utils/get-data";
+import { genreResponseType } from "@/types";
 
 type GenrePageProps = {
   searchParams: Promise<{
@@ -8,6 +10,20 @@ type GenrePageProps = {
     page: string;
     name: string;
   }>;
+};
+
+export const generateMetadata = async ({ searchParams }: GenrePageProps) => {
+  const movieGenresList: genreResponseType = await getGenresList();
+
+  const params = await searchParams;
+  const id = params.id;
+  const genreName = movieGenresList.genres.find(
+    (genre) => genre.id === Number(id)
+  );
+  console.log(genreName, "genreName");
+  return {
+    title: `MovieZ | ${genreName?.name}`,
+  };
 };
 
 const GenrePage = ({ searchParams }: GenrePageProps) => {
